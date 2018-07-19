b4452a0fa1978b60e8cb172452294c2de3388041
Rafael Takasu <rafatakasu@gmail.com>
2018-07-19 06:36:52 -0700

    basic login page template

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, flash, request, session
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-	return "hello world"
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=4000)
